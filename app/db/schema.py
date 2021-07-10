b7a1f9e6d236a5081775291c6b8e59fce56c0c99
foresto-ns <43886511+foresto-ns@users.noreply.github.com>
2021-07-10 18:03:36 +0300

    add schema.py

--- app/db/schema.py
+++ app/db/schema.py
@@ -1,8 +1,8 @@
 from sqlalchemy import (
-    Column, Integer, Float,
-    Boolean, MetaData, Table,
+    Column, Integer, Float, Date,
+    Boolean, MetaData, String, Table,
 )
-
+from sqlalchemy.orm import relationship
 
 convention = {
     'all_column_names': lambda constraint, table: '_'.join([
@@ -18,13 +18,50 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 
+clients_table = Table(
+    'clients',
+    metadata,
+    Column('client_id', Integer, primary_key=True, nullable=False, autoincrement=True),
+    Column('name', String(255), nullable=False),
+    Column('surname', String(255), nullable=False),
+    Column('phone', String(255), nullable=False),
+    Column('email', String(255), nullable=False),
+
+)
+
+products_table = Table(
+    'products',
+    metadata,
+    Column('product_id', Integer, primary_key=True, autoincrement=True),
+    Column('name', Float, nullable=False),
+    Column('price', String(255), nullable=False),
+    Column('category', String(255), nullable=False),
+    Column('availability', Boolean, nullable=False),
+)
+
 orders_table = Table(
     'orders',
     metadata,
-    Column('order_id', Integer, primary_key=True),
-    Column('weight', Float, nullable=False),
-    Column('region_id', Integer, nullable=False),
-    Column('is_ready', Boolean, nullable=False, default=0),
-    Column('complete_time', Integer, nullable=True, default=0),
-    Column('assign_id', Integer, nullable=True, default=-1),
+    Column('order_id', Integer, primary_key=True, autoincrement=True),
+    Column('client_id', Integer, nullable=False),
+    Column('date', Date, nullable=False),
+    Column('payment_method', String(255), nullable=False),
+)
+
+order_list_table = Table(
+    'order_list',
+    metadata,
+    Column('order_id', Integer),
+    Column('product_id', Integer),
+    Column('quantity', Integer)
+)
+
+orders_assign_table = Table(
+    'adresses',
+    metadata,
+    Column('client_id', Integer),
+    Column('street', String(255), nullable=False),
+    Column('city', String(255), nullable=False),
+    Column('building', String(255), nullable=False, default=0),
+    Column('flat', String(255), nullable=False, default=0),
 )
