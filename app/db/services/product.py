0695a31e807a8c82ab52fa8ad3439e68ebd452d5
oleggr <oleg.gr@outlook.com>
2021-07-18 22:10:46 +0300

    service updated to use model

--- app/db/services/product.py
+++ app/db/services/product.py
@@ -1,21 +1,19 @@
+from app.db.models.product import Product
 from app.db.schema import products_table
 from app.db.services.abstract import AbstractService
-
-
-class Args:
-    pass
 
 
 class ProductService(AbstractService):
 
     async def get_products(self):
+        res = []
+
         session = await self.get_session()
-        row = session.query(products_table) \
+        rows = session.query(products_table) \
             .filter(products_table.c.category.in_([1])) \
             .all()
 
-        if row:
-            print(row)
-            return row
-        else:
-            return False
+        for row in rows:
+            res.append(Product(**row))
+
+        return res
