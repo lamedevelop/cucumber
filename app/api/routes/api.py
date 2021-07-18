950c7e87285b67a075488895b52b728ef5e07434
Oleg Gritchin <o.gritchin@corp.mail.ru>
2021-07-18 21:20:38 +0300

    redundant response code removed

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -19,7 +19,4 @@ async def get_products(request: Request):
     service = ProductService()
     res = await service.get_products()
 
-    return JSONResponse(
-        {"result": res},
-        status_code=status.HTTP_200_OK,
-    )
+    return {"result": res}
