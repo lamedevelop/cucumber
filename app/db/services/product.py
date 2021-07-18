c89c8192f4d3e008f10c92e70e934768f84e67d3
oleggr <oleg.gr@outlook.com>
2021-07-18 22:17:34 +0300

    service updated with generator

--- app/db/services/product.py
+++ app/db/services/product.py
@@ -6,14 +6,9 @@ from app.db.services.abstract import AbstractService
 class ProductService(AbstractService):
 
     async def get_products(self):
-        res = []
-
         session = await self.get_session()
         rows = session.query(products_table) \
             .filter(products_table.c.category.in_([1])) \
             .all()
 
-        for row in rows:
-            res.append(Product(**row))
-
-        return res
+        return [Product(**row) for row in rows]
