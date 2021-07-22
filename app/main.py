f6de75fff97e50c9c6a251b8f9b28c1a6540c1be
zoglam <anzoor1998@gmail.com>
2021-07-22 22:48:12 +0300

    [hotfix] cors

--- app/main.py
+++ app/main.py
@@ -1,16 +1,29 @@
-from fastapi import FastAPI
-
 from app.api.routes.router import router
+from fastapi.middleware.cors import CORSMiddleware
+
+from fastapi import FastAPI
 
 
 def get_application() -> FastAPI:
     application = FastAPI()
     application.include_router(router)
 
+    origins = [
+        "*",
+    ]
+
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     return application
 
 
 app = get_application()
+
 
 
 # @app.on_event("startup")
