1977c37c6f0cc22071adc3cbcf6912f285a32420
oleggr <oleg.gr@outlook.com>
2021-07-25 15:26:09 +0300

    [CU-33] configs system added

---
+++ app/internal/config_loader.py
@@ -0,0 +1,18 @@
+import os
+import importlib
+
+
+class ConfigLoader:
+
+    configs_folder = 'configs'
+    default_config_name = 'default'
+    config_env_var_name = 'CUCUMBER_CONFIG'
+
+    @staticmethod
+    def getConfig():
+        """Get application config by config environment variable value."""
+
+        config_name = os.environ.get(ConfigLoader.config_env_var_name)
+        if not config_name:
+            config_name = ConfigLoader.default_config_name
+        return importlib.import_module(f'{ConfigLoader.configs_folder}.{config_name}')
